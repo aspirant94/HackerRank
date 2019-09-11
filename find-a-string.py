fd333ea29f15cf51f0f90877942e52c8b70cbc61
aspirant94 <python.developer.aspirant@gmail.com>
2019-09-11 09:31:23 +0530

    new files

--- find-a-string.py
+++ find-a-string.py
@@ -1,5 +1,10 @@
 def count_substring(string, sub_string):
-    return string.count(sub_string)
+    count = 0
+    for i in range(0, len(string)):
+        if string[i:i + len(sub_string)] == sub_string:
+            count += 1
+
+    return count
 
 
 if __name__ == '__main__':
